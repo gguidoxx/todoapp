5427adf4fd8815c16a47f8bf27b676d5f1a3faa9
gguidoxx <gudombe@gmail.com>
2022-11-09 00:14:01 -0300

    Finalizada funcionalidades de ToDo app, falta estilos perrones

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,15 @@ import ToDoApp from "./components/ToDoApp";
 import { Toaster } from "react-hot-toast";
 export default function App() {
   return (
-    <>
-      <div className=" min-h-screen flex justify-center dark:bg-zinc-900">
+    <div className="min-h-screen bg-zinc-900">
+      <h1 className="text-white text-4xl justify-center text-center">
+        To Do List
+      </h1>
+      <div className="  flex justify-center">
         <ToDoApp />
       </div>
 
       <Toaster position="bottom-center" reverseOrder={false} />
-    </>
+    </div>
   );
 }
